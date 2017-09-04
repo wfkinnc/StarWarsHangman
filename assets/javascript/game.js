b88fd24bcab422a2cfb989cecd4c7410e10f58bb
William Kassebaum <wkassebaum@rocketmail.com>
2017-09-03 20:29:47 -0400

    initial dev effort - Star Wars Hangman - 9/3/17

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+
+/* captures the key stroke */
+document.onkeyup = function(event) {
+
+      // Determines which key was pressed.
+      var userGuess = event.key;
+      alert(userGuess);
+ }     
